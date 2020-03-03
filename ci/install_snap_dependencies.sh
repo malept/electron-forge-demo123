1857980001cfd7cc39a6b14ea47096ab84bf19cb
Mark Lee <electronjs@lazymalevolence.com>
2020-03-03 12:17:26 -0800

    Install snapcraft

--- ci/install_snap_dependencies.sh
+++ ci/install_snap_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -xe
 
 if test "$(uname -s)" = "Linux"; then
-    sudo apt update
+    sudo apt-get update
+    sudo apt-get install --yes --no-install-recommends snapcraft
 
     mkdir -p fakesnap/snap
     cp ci/snapcraft.yaml fakesnap/snap/
