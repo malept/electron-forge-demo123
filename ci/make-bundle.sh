a53bfa6ebb462b05470013afd5e54e4ca405bb71
Mark Lee <electronjs@lazymalevolence.com>
2020-03-03 16:03:47 -0800

    Add support for mas & upload distributable artifacts to GHA

---
+++ ci/make-bundle.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yarn package --platform $PLATFORM
--- ci/make-distributable.sh
+++ ci/make-distributable.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 
-DEBUG=electron-forge:* yarn make --skip-package --targets=@electron-forge/maker-$MAKER
+yarn make --skip-package --targets=@electron-forge/maker-$MAKER
