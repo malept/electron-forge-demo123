2b8dfc3607488c1f86022219bd74c75958248285
Mark Lee <electronjs@lazymalevolence.com>
2020-07-02 17:55:28 -0700

    build: remove always-run apt command

--- ci/platform_setup.sh
+++ ci/platform_setup.sh
@@ -2,16 +2,15 @@
 
 case "$(uname -s)" in
     "Linux")
-        sudo apt-get install -y --no-install-recommends dpkg fakeroot rpm
         if [[ -n "$GITHUB_WORKFLOW" ]]; then
             # Work around outdated apt repository keys, etc.
             sudo rm /etc/apt/sources.list.d/bazel.list
         fi
         if [[ "$MAKER" = "deb" || -n "$APPVEYOR" ]]; then
-          sudo apt install --yes --no-install-recommends fakeroot dpkg
+          sudo apt-get install --yes --no-install-recommends fakeroot dpkg
         fi
         if [[ "$MAKER" = "rpm" || -n "$APPVEYOR" ]]; then
-          sudo apt install --yes --no-install-recommends rpm
+          sudo apt-get install --yes --no-install-recommends rpm
         fi
         if [[ "$MAKER" = "flatpak" || -n "$APPVEYOR" ]]; then
           "$(dirname $0)"/install_flatpak_dependencies.sh
