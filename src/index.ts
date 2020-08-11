acd2c4d100c054b032fef2def0716116e55a0032
Mark Lee <electronjs@lazymalevolence.com>
2020-08-10 23:51:15 -0700

    Use TypeScript+Webpack template

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow } from 'electron';
+declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
 
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
     height: 600,
-    webPreferences: {
-      contextIsolation: true,
-      disableBlinkFeatures: 'Auxclick',
-      enableRemoteModule: false,
-      nodeIntegration: false,
-      sandbox: true,
-    }
+    width: 800,
   });
 
-  // Load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
@@ -27,10 +25,10 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
