9ab3f07520f0e0b1eaf07c3a640e0b7d7823cae8
Mark Lee <electronjs@lazymalevolence.com>
2020-09-07 13:28:12 -0700

    Don't emit output for pushd/popd

--- ci/setup-windows-certificate.sh
+++ ci/setup-windows-certificate.sh
@@ -16,13 +16,13 @@ sdk_path="$windows_kit_bindir/$ver/x64"
 
 PATH="$sdk_path:$PATH"
 
-pushd "$DIR"
+pushd "$DIR" >/dev/null
 
 # Create a private key using MakeCert.exe
 MakeCert.exe -n CN=ElectronForgeCI -r -h 0 -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -e 12/31/2050 -sv codesign.pvk codesign.cer || exit 1
 
 Pvk2Pfx.exe -pvk codesign.pvk -spc codesign.cer -pfx codesign.pfx || exit 1
 
-popd
+popd >/dev/null
 
 echo "Generated test Windows certificate"
