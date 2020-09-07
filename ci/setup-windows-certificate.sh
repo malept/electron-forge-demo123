a9d805f86b92ebaf3212f83daedfeef4a1cc8f89
Mark Lee <electronjs@lazymalevolence.com>
2020-09-06 23:10:48 -0700

    hyphenated flags

--- ci/setup-windows-certificate.sh
+++ ci/setup-windows-certificate.sh
@@ -3,6 +3,8 @@
 # See: https://docs.microsoft.com/en-us/windows/win32/appxpkg/how-to-create-a-package-signing-certificate
 #
 
+set -e
+
 program_files_x86="/c/Program Files (x86)"
 windows_kit_bindir="$program_files_x86/Windows Kits/10/bin"
 ver=`(cd "$windows_kit_bindir"; ls -dr 10.*) | sed -e 1q`
@@ -11,6 +13,6 @@ sdk_path="$windows_kit_bindir/$ver/x64"
 PATH="$sdk_path:$PATH"
 
 # Create a private key using MakeCert.exe
-MakeCert.exe /n ElectronForgeCI /r /h 0 /eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" /e 12/31/2050 /sv ci/codesign.pvk ci/codesign.cer
+MakeCert.exe -n CN=ElectronForgeCI -r -h 0 -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -e 12/31/2050 -sv ci/codesign.pvk ci/codesign.cer
 
-Pvk2Pfx.exe /pvk ci/codesign.pvk /spc ci/codesign.cer /pfx ci/codesign.pfx
+Pvk2Pfx.exe -pvk ci/codesign.pvk -spc ci/codesign.cer -pfx ci/codesign.pfx
