8e6ae6439c2bd13bcef00adf74e8ab709ab7a033
Mark Lee <electronjs@lazymalevolence.com>
2020-09-07 11:20:15 -0700

    Don't assume what the cwd is

--- ci/setup-windows-certificate.sh
+++ ci/setup-windows-certificate.sh
@@ -5,6 +5,8 @@
 
 set -e
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 program_files_x86="/c/Program Files (x86)"
 windows_kit_bindir="$program_files_x86/Windows Kits/10/bin"
 ver=`(cd "$windows_kit_bindir"; ls -dr 10.*) | sed -e 1q`
@@ -13,6 +15,6 @@ sdk_path="$windows_kit_bindir/$ver/x64"
 PATH="$sdk_path:$PATH"
 
 # Create a private key using MakeCert.exe
-MakeCert.exe -n CN=ElectronForgeCI -r -h 0 -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -e 12/31/2050 -sv ci/codesign.pvk ci/codesign.cer
+MakeCert.exe -n CN=ElectronForgeCI -r -h 0 -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -e 12/31/2050 -sv "$DIR"/codesign.pvk "$DIR"/codesign.cer
 
-Pvk2Pfx.exe -pvk ci/codesign.pvk -spc ci/codesign.cer -pfx ci/codesign.pfx
+Pvk2Pfx.exe -pvk "$DIR"/codesign.pvk -spc "$DIR"/codesign.cer -pfx "$DIR"/codesign.pfx
