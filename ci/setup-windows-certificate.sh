e3f9ed90c7a964173e197dee8ed0c4a9dcb14bf5
Mark Lee <electronjs@lazymalevolence.com>
2020-09-07 13:14:02 -0700

    Try not using set -e

--- ci/setup-windows-certificate.sh
+++ ci/setup-windows-certificate.sh
@@ -3,7 +3,7 @@
 # See: https://docs.microsoft.com/en-us/windows/win32/appxpkg/how-to-create-a-package-signing-certificate
 #
 
-set -ex
+set -x
 
 if [[ -n "$APPVEYOR" ]]; then
   DIR="$(node -e "path=require('path');console.log(path.dirname(path.resolve('${BASH_SOURCE[0]}')))")"
@@ -21,8 +21,8 @@ PATH="$sdk_path:$PATH"
 pushd "$DIR"
 
 # Create a private key using MakeCert.exe
-MakeCert.exe -n CN=ElectronForgeCI -r -h 0 -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -e 12/31/2050 -sv codesign.pvk codesign.cer
+MakeCert.exe -n CN=ElectronForgeCI -r -h 0 -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -e 12/31/2050 -sv codesign.pvk codesign.cer || exit 1
 
-Pvk2Pfx.exe -pvk codesign.pvk -spc codesign.cer -pfx codesign.pfx
+Pvk2Pfx.exe -pvk codesign.pvk -spc codesign.cer -pfx codesign.pfx || exit 1
 
 popd
