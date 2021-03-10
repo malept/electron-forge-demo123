1cdbbaa0cf1f6d853e3d2aa52a40c1667a2cf6bc
Mark Lee <electronjs@lazymalevolence.com>
2021-03-09 22:35:09 -0800

    Remove unncecessary dependency installation

--- ci/platform_setup.sh
+++ ci/platform_setup.sh
@@ -7,18 +7,6 @@ case "$(uname -s)" in
             sudo rm /etc/apt/sources.list.d/bazel.list
             grep -l opensuse /etc/apt/sources.list.d/* | xargs sudo rm
         fi
-        if [[ "$MAKER" = "deb" || -n "$APPVEYOR" ]]; then
-          sudo apt-get install --yes --no-install-recommends fakeroot dpkg
-        fi
-        if [[ "$MAKER" = "rpm" || -n "$APPVEYOR" ]]; then
-          sudo apt-get install --yes --no-install-recommends rpm
-        fi
-        if [[ "$MAKER" = "flatpak" || -n "$APPVEYOR" ]]; then
-          "$(dirname $0)"/install_flatpak_dependencies.sh
-        fi
-        if [[ "$MAKER" = "snap" || -n "$APPVEYOR" ]]; then
-          "$(dirname $0)"/install_snap_dependencies.sh
-        fi
         ;;
     "Darwin")
         "$(dirname $0)"/codesign/import-testing-cert-ci.sh
