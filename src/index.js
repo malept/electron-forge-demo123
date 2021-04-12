96f330c4ebb45e29c6249cfdf7de083ad340eb6e
Mark Lee <electronjs@lazymalevolence.com>
2021-04-12 14:25:20 -0700

    Demo of relative paths

--- src/index.js
+++ src/index.js
@@ -1,48 +1,16 @@
-const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
 const path = require('path');
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      contextIsolation: true,
-      disableBlinkFeatures: 'Auxclick',
-      enableRemoteModule: false,
-      nodeIntegration: false,
-      sandbox: true,
-    }
-  });
+function getType (jsonPath) {
+  const json = JSON.parse(fs.readFileSync(jsonPath));
+  return json.type;
+}
 
-  // Load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+const subdirType = getType(path.join(__dirname, 'subdir', 'metadata.json'));
+console.log(`subdir: ${subdirType}`);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+const siblingDirType = getType(path.join(__dirname, '..', 'sibling', 'metadata.json'));
+console.log(`sibling: ${siblingDirType}`);
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+const sameDirType = getType(path.join(__dirname, 'metadata.json'));
+console.log(`same dir: ${sameDirType}`);
