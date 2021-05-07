fa023adb0cf224be3ba245359b9c28cd7437e8b6
Mark Lee <electronjs@lazymalevolence.com>
2021-05-07 16:03:34 -0700

    ci: opensuse apt sources don't exist either

--- ci/platform_setup.sh
+++ ci/platform_setup.sh
@@ -2,10 +2,6 @@
 
 case "$(uname -s)" in
     "Linux")
-        if [[ -n "$GITHUB_WORKFLOW" ]]; then
-            # Work around outdated apt repository keys, etc.
-            grep -l opensuse /etc/apt/sources.list.d/* | xargs sudo rm
-        fi
         if [[ "$MAKER" = "flatpak" || -n "$APPVEYOR" ]]; then
           "$(dirname $0)"/install_flatpak_dependencies.sh
         fi
