c14cef0aa4ab718894c3178553505cba329d5d72
Mark Lee <electronjs@lazymalevolence.com>
2021-05-07 16:00:58 -0700

    ci: bazel doesn't exist anymore in the GitHub Actions runner it seems

--- ci/platform_setup.sh
+++ ci/platform_setup.sh
@@ -4,7 +4,6 @@ case "$(uname -s)" in
     "Linux")
         if [[ -n "$GITHUB_WORKFLOW" ]]; then
             # Work around outdated apt repository keys, etc.
-            sudo rm /etc/apt/sources.list.d/bazel.list
             grep -l opensuse /etc/apt/sources.list.d/* | xargs sudo rm
         fi
         if [[ "$MAKER" = "flatpak" || -n "$APPVEYOR" ]]; then
