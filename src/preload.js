c01ba54227f0611f8fdc9e9e618ad5dc66aae4a8
Mark Lee <electronjs@lazymalevolence.com>
2021-06-13 08:55:53 -0700

    Make it more obvious if the preload succeeds

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const packageJSON = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'package.json')));
-console.log(`Package metadata (preload): Name="${packageJSON.name}", Version="${packageJSON.version}"`);
+window.addEventListener('DOMContentLoaded', () => {
+  const packageJSON = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'package.json')));
+  document.body.innerText = `Package metadata (preload): Name="${packageJSON.name}", Version="${packageJSON.version}"`;
+});
